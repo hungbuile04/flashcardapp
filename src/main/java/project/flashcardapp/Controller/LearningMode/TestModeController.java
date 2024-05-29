c34fe013df4547ddfa1468f4cfb662b39be01981
rye0812 <147314702+rye0812@users.noreply.github.com>
2024-05-30 03:00:16 +0700

    new(29/5) testmode

--- src/main/java/project/flashcardapp/Controller/LearningMode/TestModeController.java
+++ src/main/java/project/flashcardapp/Controller/LearningMode/TestModeController.java
@@ -1,5 +1,6 @@
 package project.flashcardapp.Controller.LearningMode;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -32,15 +33,42 @@ public class TestModeController implements Initializable {
     @FXML
     private Label questionLabel;
 
+    @FXML
+    private Label resultAlert;
+
+    @FXML
+    private Label showAnswer;
+
+    @FXML
+    private Button checkButton;
+
+    @FXML
+    private Button showAnswerButton;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.deck = DeckInfoController.deck;
+        updateCard();
+    }
+
+    private void updateCard() {
+        if (deck.getCards().getSize() == 0) {
+            System.out.println("Card list is empty!");
+            return;
+        }
+
+        questionLabel.setText(deck.getCards().getCard(currentIndex).getQuestion());
+        questionLabel.setVisible(true);
+        answerField.clear();
+        resultAlert.setText("");
+        showAnswer.setText("");
     }
 
     @FXML
     void showNextCard(MouseEvent event) {
         if (currentIndex < deck.getCards().getSize() - 1) {
             currentIndex++;
+            updateCard();
         }
     }
 
@@ -48,7 +76,23 @@ public class TestModeController implements Initializable {
     void showPreviousCard(MouseEvent event) {
         if (currentIndex > 0) {
             currentIndex--;
+            updateCard();
         }
     }
+    public void checkAnswer(ActionEvent event){
+        String as = answerField.getText();
+        if (as.equals(deck.getCards().getCard(currentIndex).getAnswer())) {
+            resultAlert.setText("CORRECT");
+            resultAlert.setVisible(true);
+        }else{
+            resultAlert.setText("WRONG");
+            resultAlert.setVisible(true);
+        }
+    }
+
+    public void showAnswer(MouseEvent event){
+        showAnswer.setText(deck.getCards().getCard(currentIndex).getAnswer());
+    }
+
 
 }
