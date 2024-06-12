29ba97cbb91fd5e5de5cf1c668215ea46264ede3
tmtuan04 <tuan04tm@gmail.com>
2024-06-12 21:40:13 +0700

    tuan save settings 50%

--- src/main/java/project/flashcardapp/Controller/Display/SettingsController.java
+++ src/main/java/project/flashcardapp/Controller/Display/SettingsController.java
@@ -2,7 +2,10 @@ package project.flashcardapp.Controller.Display;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
@@ -19,38 +22,49 @@ public class SettingsController {
     @FXML
     private AnchorPane scenePane;
 
-//    Back to MainWindow
+    @FXML
+    private Circle profileImage;
+
     @FXML
     public void backtoMainButton(ActionEvent event) throws IOException {
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-//        Thêm cửa sổ cảnh báo, nếu có sự thay đổi ở cài đặt thì hỏi có lưu hay không
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Cài đặt");
         alert.setHeaderText("Bạn đã thay đổi phần cài đặt");
         alert.setContentText("Bạn có muốn lưu lại thay đổi không?");
         if (alert.showAndWait().get() == ButtonType.OK) {
-            stage = (Stage)scenePane.getScene().getWindow();
+            stage = (Stage) scenePane.getScene().getWindow();
             stage.close();
         }
     }
-
-//    Upload Image
-
-    @FXML
-    private Circle profileImage;
 
     @FXML
     public void uploadImage(ActionEvent event) throws IOException {
         FileChooser fileChooser = new FileChooser();
 
-        Stage stage = (Stage)scenePane.getScene().getWindow();
+        Stage stage = (Stage) scenePane.getScene().getWindow();
 
         File file = fileChooser.showOpenDialog(stage);
 
         if (file != null) {
-            Image image = new Image(file.toURI().toString(), 300, 300 ,false,true);
+            Image image = new Image(file.toURI().toString(), 300, 300, false, true);
             profileImage.setFill(new ImagePattern(image));
         }
+    }
+
+    @FXML
+    public void switchtoSettingsUser(ActionEvent event) throws IOException {
+
+        Stage currentStage = (Stage) scenePane.getScene().getWindow();
+        currentStage.close();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/project/flashcardapp/user_information.fxml"));
+        Parent settingsSceneRoot = loader.load();
+        Scene settingsScene = new Scene(settingsSceneRoot);
+        Stage stage = new Stage();
+        stage.setTitle("User Information");
+        stage.setScene(settingsScene);
+        stage.show();
     }
 }
---
+++ src/main/java/project/flashcardapp/Controller/Display/UserInformation.java
@@ -0,0 +1,38 @@
+package project.flashcardapp.Controller.Display;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class UserInformation {
+
+    @FXML
+    private Label Age;
+
+    @FXML
+    private Label Email;
+
+    @FXML
+    private Label Phone;
+
+    @FXML
+    private Label Username;
+
+    @FXML
+    private TextField ageField;
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private Label userTitle;
+
+
+}
